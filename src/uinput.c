f3bd9239ae6b8257c48b8c61547ad9f464e6265c
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2022-03-23 17:40:50 +0100

    fix indent and whitespace in src/uinput.c
    - drop trailing whitespace
    - use 4 spaces for indent consistently

--- src/uinput.c
+++ src/uinput.c
@@ -20,19 +20,19 @@ extern int motion_interval;
 
 void emit(int fd, int type, int code, int val)
 {
-   struct input_event ie;
-   memset(&ie, 0, sizeof(ie)); 
-   gettimeofday(&ie.time, NULL); 
+    struct input_event ie;
+    memset(&ie, 0, sizeof(ie));
+    gettimeofday(&ie.time, NULL);
 
-   ie.type = type;
-   ie.code = code;
-   ie.value = val;
-   /* timestamp values below are ignored */
+    ie.type = type;
+    ie.code = code;
+    ie.value = val;
+    /* timestamp values below are ignored */
 /*   ie.time.tv_sec = 0;
-   ie.time.tv_usec = 0;
+    ie.time.tv_usec = 0;
 */
-   int ret = write(fd, &ie, sizeof(ie));
-   return;
+    int ret = write(fd, &ie, sizeof(ie));
+    return;
 }
 
 
@@ -62,7 +62,7 @@ void fake_key_uinput(int fd, char *keyname, int state)
         token = strtok_r(NULL, "+", &end_token);
     }
     free(keys);
-} 
+}
 
 void fake_mouse_button_uinput(int fd, int button_number, int state)
 {
@@ -115,32 +115,32 @@ int init_uinput()
         exit(1);
     }
 
-    ioctl(fd, UI_SET_EVBIT, EV_KEY); 
+    ioctl(fd, UI_SET_EVBIT, EV_KEY);
     ioctl(fd, UI_SET_EVBIT, EV_REL);
     ioctl(fd, UI_SET_RELBIT, REL_X);
     ioctl(fd, UI_SET_RELBIT, REL_Y);
     int i;
     /* enable all keycodes support */
-    for (i=0; i < 256; i++) { 
-        ioctl(fd, UI_SET_KEYBIT, i); 
-    } 
-  
-    /* enable all mouse buttons */ 
-    ioctl(fd, UI_SET_KEYBIT, BTN_MOUSE); 
-    ioctl(fd, UI_SET_KEYBIT, BTN_TOUCH); 
-    ioctl(fd, UI_SET_KEYBIT, BTN_MOUSE); 
-    ioctl(fd, UI_SET_KEYBIT, BTN_LEFT); 
-    ioctl(fd, UI_SET_KEYBIT, BTN_MIDDLE); 
-    ioctl(fd, UI_SET_KEYBIT, BTN_RIGHT); 
-    ioctl(fd, UI_SET_KEYBIT, BTN_FORWARD); 
-    ioctl(fd, UI_SET_KEYBIT, BTN_BACK); 
-    
+    for (i=0; i < 256; i++) {
+        ioctl(fd, UI_SET_KEYBIT, i);
+    }
+
+    /* enable all mouse buttons */
+    ioctl(fd, UI_SET_KEYBIT, BTN_MOUSE);
+    ioctl(fd, UI_SET_KEYBIT, BTN_TOUCH);
+    ioctl(fd, UI_SET_KEYBIT, BTN_MOUSE);
+    ioctl(fd, UI_SET_KEYBIT, BTN_LEFT);
+    ioctl(fd, UI_SET_KEYBIT, BTN_MIDDLE);
+    ioctl(fd, UI_SET_KEYBIT, BTN_RIGHT);
+    ioctl(fd, UI_SET_KEYBIT, BTN_FORWARD);
+    ioctl(fd, UI_SET_KEYBIT, BTN_BACK);
+
     memset(&usetup, 0, sizeof(usetup));
     usetup.id.bustype = BUS_USB;
     usetup.id.vendor = 0x1234; /* sample vendor */
     usetup.id.product = 0x5678; /* sample product */
     strcpy(usetup.name, "Enjoy device");
-    
+
     ioctl(fd, UI_DEV_SETUP, &usetup);
     ioctl(fd, UI_DEV_CREATE);
     return fd;
@@ -148,6 +148,6 @@ int init_uinput()
 
 void close_uinput(int fd)
 {
-   ioctl(fd, UI_DEV_DESTROY);
-   close(fd);
+    ioctl(fd, UI_DEV_DESTROY);
+    close(fd);
 }
